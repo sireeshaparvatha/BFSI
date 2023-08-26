53eea1fa5d370afa845c178e2a1235d1505c9cfe
baburao <rao.babu5@gmail.com>
2023-08-26 14:51:05 +0530

    initial

---
+++ src/main/java/com/bfsi/BfsiApplication.java
@@ -0,0 +1,13 @@
+package com.bfsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bfsi/BfsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
